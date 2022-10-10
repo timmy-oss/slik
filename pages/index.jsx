931ca9f2c3afed90865973a41ca49f40970279b8
timmy-oss <timileyinpelumi.dev@gmail.com>
2022-10-10 12:26:18 +0100

    Init - index.js rewritten

---
+++ pages/index.jsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div className=" min-h-screen bg-main-bg  ">
+      <Head>
+        <title> Slik </title>
+        <link rel="icon" href="/assets/slik-favicon-transparent-web.png" />
+        <link
+          rel="shortcut icon"
+          href="/assets/slik-social-red-background.png"
+        />
+      </Head>
+      <main className="flex w-full min-h-screen  flex-col items-center justify-center  text-center">
+        <div className=" self-center h-[100%] ">
+          <div className="hidden   md:block">
+            <Image
+              src="/assets/Slik-Logo-horizontal-web.png"
+              layout="intrinsic"
+              width="300"
+              height="200"
+              alt="logo"
+              objectFit="contain"
+              className="animate-pulse "
+            />
+          </div>
+
+          <div className="md:hidden block">
+            <Image
+              src="/assets/slik-favicon-transparent-web.png"
+              layout="intrinsic"
+              width="200"
+              height="100"
+              alt="logo"
+              objectFit="contain"
+              className="animate-pulse "
+            />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-bg': '#ffffff',
+        'prim-color': '#ee3a46',
+        'sec-color': '#d4af37',
+        'c-1': '#0f6194',
+        'c-2': '#0b8fe0'
+      },
+    },
   },
   plugins: [],
 }
