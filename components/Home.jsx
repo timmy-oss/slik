aaab69d043a405ed6219f581acb7f20b08396342
timmy-oss <timileyinpelumi.dev@gmail.com>
2022-10-11 16:53:08 +0100

    updates

--- components/Home.jsx
+++ components/Home.jsx
@@ -9,11 +9,20 @@ import {
   faShieldHalved,
   faStopwatch,
   faArrowDownUpLock,
+  faCircleInfo,
 } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import AOS from "aos";
 
 export default function Home(props) {
   const router = useRouter();
+
+  AOS.init({
+    offset: 80,
+    duration: 800,
+    easing: "ease-in-sine",
+    delay: 100,
+  });
 
   return (
     <>
@@ -22,14 +31,16 @@ export default function Home(props) {
       <div className="min-h-screen bg-gray-100 mt-14">
         <div className="px-4 md:px-24 bg-local  bg-origin-border bg-clip-border bg-top md:bg-[right_top_150px]  bg-no-repeat bg-[length:100%_300px] md:bg-[length:700px_500px] bg-[#8C8C8C] min-h-[800px] py-12 bg-[url('/assets/slik-delivery-red.jpg')] pt-[320px] md:pt-[150px] ">
           <h1
+            data-aos="fade-up"
             style={{ fontFamily: "Work Sans" }}
-            className="text-center capitalize p-4 font-black text-4xl lg:text-7xl 2xl:text-7xl max-w-3xl 2xl:max-w-4xl lg:text-left  text-c-1"
+            className="text-center capitalize p-4 font-black text-4xl lg:text-6xl 2xl:text-6xl max-w-3xl 2xl:max-w-4xl lg:text-left  text-c-1"
           >
             {" "}
             Reliable delivery service every time{" "}
           </h1>
 
           <h1
+            data-aos="fade-up"
             style={{ fontFamily: "Work Sans" }}
             className="text-center capitalize p-4 font-black text-3xl lg:text-4xl 2xl:text-5xl max-w-3xl 2xl:max-w-4xl lg:text-left  text-prim-color/80"
           >
@@ -37,12 +48,16 @@ export default function Home(props) {
           </h1>
 
           <h1
+            data-aos="fade-up"
             style={{ fontFamily: "Work Sans" }}
-            className="text-center capitalize p-4 font-black text-4xl lg:text-7xl 2xl:text-7xl max-w-3xl 2xl:max-w-4xl lg:text-left  text-c-1"
+            className="text-center capitalize p-4 font-black text-4xl lg:text-6xl 2xl:text-6xl max-w-3xl 2xl:max-w-4xl lg:text-left  text-c-1"
           >
             Experience the modern way of logistics with Slik.
           </h1>
-          <div className="mx-auto md:hidden text-center md:text-left text-c-1 text-4xl md:text-5xl my-2 md:my-6 px-4  space-x-8 ">
+          <div
+            data-aos="fade-up"
+            className="mx-auto md:hidden text-center md:text-left text-c-1 text-4xl md:text-5xl my-2 md:my-6 px-4  space-x-8 "
+          >
             <FontAwesomeIcon
               icon={faStopwatch}
               className="text-4xl md:text-5xl"
@@ -59,40 +74,93 @@ export default function Home(props) {
             />
           </div>
           <p
-            style={{ fontFamily: "Roboto" }}
+            data-aos="fade-up"
+            style={{ fontFamily: "Work Sans" }}
             className="text-center max-w-3xl px-4 text-lg lg:text-xl lg:text-left  py-4 "
           >
-            {" "}
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
-            in alias perspiciatis. Similique iure eveniet hic maiores vero quod
-            assumenda libero voluptatum id, sequi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+            obcaecati saepe dicta explicabo dolorum corporis vel repudiandae qui
+            sint aut, soluta recusandae repellendus enim asperiores incidunt,
+            reiciendis dolorem eaque eveniet doloremque cum excepturi.
           </p>
 
-          <h1
-            style={{ fontFamily: "Work Sans" }}
-            className="text-center  mt-8 md:mt-16 capitalize p-4 font-black text-3xl lg:text-4xl 2xl:text-5xl  lg:text-left  text-black"
-          >
-            Join the waitlist to get notified when we launch
-          </h1>
+          <div data-aos="fade-up">
+            <h1
+              style={{ fontFamily: "Work Sans" }}
+              className="text-center  mt-8 md:mt-16 capitalize p-4 font-black text-3xl lg:text-4xl 2xl:text-5xl  lg:text-left  text-black"
+            >
+              Join the waitlist to get notified when we launch
+            </h1>
 
-          <div className="my-8 px-2 md:px-4 md:w-[70%]">
-            <div className="flex flex-col md:flex-row justify-around space-y-8 md:space-y-0 md:space-x-16">
-              <div className="flex flex-row w-full hover:bg-prim-color/80 bg-prim-color rounded-xl  justify-between items-center space-x-2 ">
-                <input
-                  className=" outline-none w-[65%] text-base md:text-lg h-[50px] caret:text-prim-color text-prim-color placeholder:text-prim-color/50  py-2 pl-8 pr-8 rounded-y-xl rounded-l-xl  "
-                  type="email"
-                  placeholder="johndoe@gmail.com"
-                  name="waiter"
-                />
-                <button className="block w-[35%] text-base text-white font-bold outline-none  rounded-y-xl rounded-r-xl text-center ">
-                  Join Waitlist
-                </button>
+            <div className="my-8 px-2 md:px-4 md:w-[70%]">
+              <div className="flex flex-col md:flex-row justify-around space-y-8 md:space-y-0 md:space-x-16">
+                <div className="flex flex-row w-full hover:bg-prim-color/80 bg-prim-color rounded-xl  justify-between items-center space-x-2 ">
+                  <input
+                    className=" outline-none w-[65%] text-base md:text-lg h-[50px] caret:text-prim-color text-prim-color placeholder:text-prim-color/50  py-2 pl-8 pr-8 rounded-y-xl rounded-l-xl  "
+                    type="email"
+                    placeholder="johndoe@gmail.com"
+                    name="waiter"
+                  />
+                  <button className="block w-[35%] text-base text-white font-bold outline-none  rounded-y-xl rounded-r-xl text-center ">
+                    Join Waitlist
+                  </button>
+                </div>
+
+                <div className="w-full">
+                  <button className="block w-full text-xl font-bold px-6 py-2 hover:bg-c-1/80 transition-colors transform duration-300 h-[50px]   bg-c-1 ring-[2px] ring-c-1 text-white rounded-xl outline-none text-center ">
+                    Learn More &nbsp;{" "}
+                    <FontAwesomeIcon className="text-2xl" icon={faCircleInfo} />
+                  </button>
+                </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-              <div className="w-full">
-                <button className="block w-full text-xl font-bold px-6 py-2 hover:bg-c-1/80 transition-colors transform duration-300 h-[50px]   bg-c-1 ring-[2px] ring-c-1 text-white rounded-xl outline-none text-center ">
-                  Get Started
-                </button>
+      {/* Frame 2  */}
+
+      <div className="min-h-screen  bg-gray-100">
+        <div className="px-4 w-full  md:px-24 bg-local  bg-origin-border bg-clip-border bg-top md:bg-[left_top_150px]  bg-no-repeat bg-[length:100%_300px] md:bg-[length:700px_500px] bg-[#8C8C8C] min-h-[800px] py-12 bg-[url('/assets/slik-delivery-white.jpg')] pt-[320px] md:pt-[150px] ">
+          <div className="md:ml-[45%]">
+            <h1
+              data-aos="fade-up"
+              style={{ fontFamily: "Work Sans" }}
+              className="text-center capitalize p-4 font-black text-4xl lg:text-6xl 2xl:text-6xl  lg:text-left   text-white"
+            >
+              Slik provides clean and fast logistics service
+            </h1>
+
+            <h1
+              data-aos="fade-up"
+              style={{ fontFamily: "Work Sans" }}
+              className="text-center capitalize p-4 font-black text-2xl lg:text-3xl 2xl:text-4xl  lg:text-left  text-c-1"
+            >
+              Want to contact us? Our channels are always available 24 hours a
+              day, 7 days a week!
+            </h1>
+
+            <p
+              data-aos="fade-up"
+              style={{ fontFamily: "Work Sans" }}
+              className="text-center max-w-3xl px-4 text-lg lg:text-xl lg:text-left  py-4 "
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              obcaecati saepe dicta explicabo dolorum corporis vel repudiandae
+              qui sint aut, soluta recusandae repellendus enim asperiores
+              incidunt, reiciendis dolorem eaque eveniet doloremque cum
+              excepturi.
+            </p>
+
+            <div data-aos="fade-up">
+              <div className="my-8 px-2 md:px-4 md:w-[70%]">
+                <div className="flex flex-col md:flex-row justify-around space-y-8 md:space-y-0 md:space-x-16">
+                  <div className="w-full">
+                    <button className="block w-full text-xl font-bold px-6 py-2 hover:bg-c-1/80 transition-colors transform duration-300 h-[50px]   bg-c-1 ring-[2px] ring-c-1 text-white rounded-xl outline-none text-center ">
+                      Call Us
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
+import "aos/dist/aos.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
 config.autoAddCss = false;
