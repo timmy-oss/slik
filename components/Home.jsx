29907448b7e24cecb9809e3c242be35fef396d98
timmy-oss <timileyinpelumi.dev@gmail.com>
2022-10-13 03:51:35 +0100

    Overhaul of homepage - desktop [ dark mode support]

--- components/Home.jsx
+++ components/Home.jsx
@@ -28,7 +28,7 @@ export default function Home(props) {
       <DesktopNavBar />
       <MobileNavBar />
       <div className="bg-[#EE3A46] md:bg-[#FBF7EB] dark:bg-[#EE3A46] md:dark:bg-[#242318]   md:pt-32 lg:pt-14 flex flex-col lg:flex-row justify-center lg:justify-around  min-h-[727px]">
-        <div className=" w-full bg-[#FBF7EB] dark:bg-[#EE3A46] md:dark:bg-[#242318]   lg:w-[60%] px-4 lg:px-32 pt-24 pb-16 lg:pb-[67px]">
+        <div className="bg-[#EE3A46] w-full md:bg-[#FBF7EB] dark:bg-[#EE3A46] md:dark:bg-[#242318]   lg:w-[60%] px-4 lg:px-32 pt-24 pb-16 lg:pb-[67px]">
           <h1
             data-aos="fade-up"
             style={{ fontFamily: "Work Sans" }}
