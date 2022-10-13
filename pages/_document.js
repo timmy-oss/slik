2034a1641ebcc9136d8c54a35eccd35ed20a3216
timmy-oss <timileyinpelumi.dev@gmail.com>
2022-10-13 03:05:32 +0100

    Overhaul of homepage - desktop [ dark mode support]

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,8 @@ export default function Document() {
 
                 <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)" />
                 <meta name="theme-color" content="#242318" media="(prefers-color-scheme: dark)" />
-
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Mulish:wght@400;700;900&family=Roboto:wght@400;700;900&family=Work+Sans:wght@400;700;900&display=swap" rel="stylesheet" />
